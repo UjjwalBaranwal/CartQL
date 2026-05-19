627c7bd09f5d105eda443ebc98abfe18cff5790f
ujjwal <ujjwalbaranwal221715@gmail.com>
2026-05-19 22:40:21 +0530

    added database and logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,30 @@
 // Package main is the entry point for the CartQL API server.
 package main
 
-import "fmt"
+import (
+	"github.com/UjjwalBaranwal/CartQL/internal/config"
+	"github.com/UjjwalBaranwal/CartQL/internal/database"
+	"github.com/UjjwalBaranwal/CartQL/internal/logger"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
+	log := logger.New()
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to load config")
+	}
+	db, err := database.New(&cfg.Database)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to connect to database")
+	}
+	mainDB, err := db.DB()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to get database connection")
+	}
+	defer mainDB.Close()
+	log.Info().Msg("Database connection established")
+	gin.SetMode(cfg.Server.GinMode)
+	log.Info().Msg("Starting Server")
 }
